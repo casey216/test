f4b575659b81e82fca45aadfeee742b29aeeb52c
casey216 <caseynzewi@gmail.com>
2023-03-27 23:18:41 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -89,3 +89,37 @@ int _print_int(va_list ls)
 	}
 	return (len);
 }
+
+/**
+ * _print_binary - Prints a number in binary
+ * @ls: list of arguments, va_list
+ *
+ * Return: the number of printed chars, int
+ */
+int _print_binary(va_list ls)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+	
+	n = va_arg(ls, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
+}
