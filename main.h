74c17366eaec9b58926c8b98e5592f4046d2836c
casey216 <caseynzewi@gmail.com>
2023-03-27 22:36:59 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,12 +1,27 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-#include <stddef.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct intface - interface for printing
+ * @id: the character, which identifies the type of
+ * the argument
+ * @fn: function to run if id matches
+ */
+typedef struct intface
+{
+	char id;
+	int (*fn)(va_list);
+} interface;
 
 int _printf(const char *format, ...);
-int _strlen(const char *str);
-int _putchar(char c);
-int print_decimal(int n);
-int print_string(char *str);
+int _print_char(va_list ls);
+int _print_mod(va_list ls);
+int _print_string(va_list ls);
+int _print_int(va_list ls);
+
 
 #endif
