4a7df01e36430974c78c9094f4ff9a06996e0384
casey216 <caseynzewi@gmail.com>
2023-03-28 00:14:31 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -27,6 +27,10 @@ int _printf(const char *format, ...)
 	
 	if (format == NULL)
 		return (-1);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 
 	va_start(lst, format);
 	for (i = 0; format[i]; i++)
