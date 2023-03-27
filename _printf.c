45ac677b2f8e396b3428c29f059bcdfaefd2a610
Kenechi Nzewi <caseynzewi@gmail.com>
2023-03-27 05:04:28 -0700

    created gitignore

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ *
+ * @format: character string. It is composed of
+ * zero or more directives.
+ *
+ * Return: The number of char printed. 
+ */
+
+int _printf(const char *format, ...)
+{
+}
